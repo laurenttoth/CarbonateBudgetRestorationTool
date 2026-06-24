afd7052a3e7d044b03a9017f86ee9a764d2c47cc
Toth <ltoth@usgs.gov>
2026-06-24 15:11:35 -0400

    Added script to calculate parrotfish, urchin, and sponge bioerosion from user-input data

--- R/RestorationMonitoring.R
+++ R/RestorationMonitoring.R
@@ -25,10 +25,73 @@ Substrate<-Monitoring_data %>%
 #Calculate microbioerosion
 Substrate$Microbioerosion<-(Substrate$Substrate/100)*0.24
 
+#If no data provided or if incomplete data provided (e.g. parrotfish but no sponges)
 #Add bioerosion for each site
 Monitoring_data_site<-Monitoring_data_site %>%
-  left_join(Bioerosion, by = c("Habitat", "Subegion") #add bioerosion
+  left_join(Bioerosion, by = c("Habitat", "Subegion")) %>% #add bioerosion
   left_join(Substrate, by = c("Habitat", "Subegion"))
+
+#If bioerosion input data provided apply the following calculations, as relevant
+#If data available for every timepoint in the Monitoring_data apply timepoint specific rates
+#Else apply single value or average value from whatever is provided
+
+#Parrotfish Bioerosion
+#ParrotfishBioerosionRates.csv has species, size class, and life-phase-specific bioerosion rates
+#Parrotfish from user input
+#remove juveniles, which are assumed to contribute minimally to bioerosion
+Parrotfish<-Parrotfish[Parrotfish$Size!="0-9",]
+#Create a variable that merges text for life phase and size to align with rates table
+Parrotfish$PhaseSize<-paste(Parrotfish$Life_phase, Parrotfish$Fork_length_cm, sep="")
+
+ParrotfishRates<-read.csv("ParrotfishBioerosionRates.csv", header=T)
+ParrotfishRates<-na.omit(ParrotfishRates)
+
+#merge rates and parrtofish data
+Parrotfish_rates<-merge(Parrotfish, ParrotfishRates, by = c("Taxon","PhaseSize"), all.x = T)
+Parrotfish_rates<-Parrotfish_rates[1:588,]
+
+#Calculate bioerosion from each individual parrotfish observed
+Parrotfish_rates$Bioerosion<-(Parrotfish_rates$Count*Parrotfish_rates$Rate)/Parrotfish_rates$Survey_Area_m2
+
+#Sum for site and/or timepoint
+Parrotfish_rates_site<-Parrotfish_rates %>%
+  group_by(Unique_Site_ID, Years_Post_Restoration) %>%
+  summarize(SumBioerosion=sum(Bioerosion, na.rm=T), SumCounts=sum(Count))
+
+#Urchin Bioerosion
+#UrchinBioerosionRates.csv has species an test-size specific bioerosion rates
+#test sizes are median of range in input
+#If test size not incuded use 30 cm as most urching on FCR are relatively small
+UrchinRates<-read.csv("UrchinBioerosionRates.csv", header=T)
+#Urchins from user input
+UrchinBioerosion<-merge(Urchins, UrchinRates, by = c("Taxon","Test.size"), all.x = T)
+
+#Calcualte individual bioerosion of all urchins
+#Survey_Area_m2 from input data
+UrchinBioerosion$Bioerosion<-((Urchins$Count/Urchins$Survey_Area_m2)*UrchinBioerosion$Bioerosion.rate*365)/1000
+
+#Sum for site and/or timepoint
+Urchin_rates_site<-UrchinBioerosion %>%
+  group_by(Unique_Site_ID, Years_Post_Restoration) %>%
+  summarize(SumBioerosion=sum(Bioerosion, na.rm=T))
+
+#Sponge Bioerosion
+#Sponge from user input
+#SpongeBioerosionRates.csv has species-specific sponge bioerosion rates
+SpongeRates<-read.csv("SpongeBioerosionRates.csv", header=T)
+Sponge_rates <- merge(Sponges, SpongeRates, by = "Taxon", all.x = T)
+
+#convert sponge areas in cm2 to m2
+Sponge_rates$Area<-Sponge_rates$Area*0.0001
+
+#Calcuate bioerosion by each observed sponge
+#Survey_Area_m2 from input data
+Sponge_rates$Bioerosion <- (Sponge_rates$Area * Sponge_rates$Bioerosion.rate)/Sponge_rates$Survey_Area_m2
+
+#Sum for site and/or timepoint
+Sponge_rates_site<-Sponge_rates %>%
+  group_by(Unique_Site_ID, Years_Post_Restoration) %>%
+  summarize(SumBioerosion=sum(Bioerosion, na.rm=T))
 
 #Calculate net budget
 Monitoring_data_site$NP<-Monitoring_data_site$GP-sum(Monitoring_data_site$Microbioerosion, Monitoring_data_site$Parrotfish, Monitoring_data_site$Urchins, Monitoring_data_site$BioSponges)
